557e9a1f14e011bcdf16b2b5b3ccade7984e2590
Darlan0307 <darlanchagas2020@gmail.com>
2025-04-24 20:25:11 -0300

    usando django templates

--- rdf/customer/urls.py
+++ rdf/customer/urls.py
@@ -11,11 +11,16 @@
 
 from django.urls import include, path
 from rest_framework import routers
-from .views import CustomerViewSet
+from .views import CustomerViewSet, CustomerListTemplateView,CustomerFormView, CustomerDeleteView
+
 
 router = routers.DefaultRouter()
 router.register(r'customers', CustomerViewSet, basename='customer')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('clientes/', CustomerListTemplateView.as_view(), name='list-customers'),
+    path('clientes/novo/', CustomerFormView.as_view(), name='customer_create'),
+    path('clientes/<int:pk>/editar/', CustomerFormView.as_view(), name='customer_edit'),
+    path('clientes/<int:pk>/excluir/', CustomerDeleteView.as_view(), name='customer_delete'),
 ]
--- rdf/customer/views.py
+++ rdf/customer/views.py
@@ -1,7 +1,10 @@
+from django.views import View
 from rest_framework import viewsets, filters
 from .models import Customer
 from .serializers import CustomerSerializer
 from rest_framework.pagination import PageNumberPagination
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView
 
 class CustomerPagination(PageNumberPagination):
     page_size = 10
@@ -15,3 +18,17 @@ class CustomerViewSet(viewsets.ModelViewSet):
     search_fields = ['name', 'cpf_cnpj', 'email']
     ordering_fields = ['name', 'qtd_seller']
 
+class CustomerListTemplateView(TemplateView):
+    template_name = 'customers/list.html'
+
+class CustomerFormView(View):
+    def get(self, request, pk=None):
+        customer = None
+        if pk:
+            customer = get_object_or_404(Customer, pk=pk)
+        return render(request, 'customers/form.html', {'customer': customer or {}})
+
+class CustomerDeleteView(View):
+    def get(self, request, pk):
+        customer = get_object_or_404(Customer, pk=pk)
+        return render(request, 'customers/confirm_delete.html', {'customer': customer})
